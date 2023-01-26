4ec8d63deee6ba9dad38db56dc630c97d8d3a0d6
Your Name <you@example.com>
2023-01-26 13:06:10 +0500

    git commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// //1 masala
+// const count = 5
+
+// function name(count) {
+//     const arr = [2,93,8,9,5,1,4,6,8,9]
+//     const upCount = arr.sort()
+//     console.log(upCount);
+//     console.log(upCount.indexOf(count));   
+//     const downCount = arr.sort((a,b)=> b-a)
+//     console.log(downCount);
+//     console.log(downCount.indexOf(count));
+// }
+// name(count);
+
+//  //2 masala
+// let num = 5
+// function newfuction (num) {
+//     let number = [1,4,5,66,7,8]
+//     let result = Math.max(...number)
+//     let result1 = Math.min(...number)
+//     let a = result - num
+//     let b = num - result1
+//     console.log(a); 
+//     console.log(b); 
+// }
+// newfuction(num);
+
+// 3 masala
+// let params = "h" 
+// function addfuction(params) {
+//     let obj = {1: "a", 2: "b", 7: "c" }
+//     let objval = Object.values(obj)
+//     objval.push(params)
+//     objval.unshift(params)
+//     console.log(objval);
+// }
+// addfuction(params);
+
+// 4 masala
+// let params1 = "c"
+// function addfuction(params1) {
+//     let obj1 = {1: "a", 2: "b", 7: "c" }
+//     let objval1 = Object.values(obj1)
+//     console.log(objval1);
+//     let result = objval1.includes(params1)
+//     if(result){
+//        console.log(`${result} bu values bor`);
+//     }else{
+//         console.log(`${result} bu values yuq`);
+//     }
+// }
+// addfuction(params1);
